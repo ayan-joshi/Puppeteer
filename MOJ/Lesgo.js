6ecaf4b55459092ee3f69633f624b5651e557cee
ayan-joshi <ayanjoshi182003@gmail.com>
2024-03-07 20:23:37 +0530

    commit

---
+++ MOJ/Lesgo.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+// Read the JSON file synchronously
+const rawData = fs.readFileSync('input.json');
+
+// Parse JSON data
+const jsonData = JSON.parse(rawData);
+
+// Extracting user_ids from the users object
+const userIds = Object.keys(jsonData.users);
+
+// Create an object to store user_ids with corresponding video URLs
+const outputData = {};
+
+userIds.forEach(userId => {
+    const user = jsonData.users[userId];
+    const videos = jsonData.videos.filter(video => video.user_id === parseInt(userId)); // Filter videos by user_id
+
+    // Extracting the username
+    const username = user.username;
+    
+    // Extracting the video URLs
+    const videoUrls = videos.map(video => `https://triller.co/@${username}/video/${video.video_uuid}`);
+    
+    // Store the username and video URLs in the outputData
+    outputData[username] = videoUrls;
+});
+
+// Stringify the outputData
+const outputJson = JSON.stringify(outputData, null, 2);
+
+// Write to output.json
+fs.writeFileSync('output.json', outputJson);
+
+console.log('Data stored in output.json');
